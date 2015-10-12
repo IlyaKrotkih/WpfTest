41ce29ba1f1a2f8b7d4bc16fb0afd0d46f4986e7
Илья Кротких <ivashca99@gmail.com>
2015-10-12 14:21:52 +0300

    First Commit

---
+++ WpfTest/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfTest
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int number;
+
+        public MainWindow()
+        {
+            number = 0;
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnClickMe_Click(object sender, RoutedEventArgs e)
+        {
+            lstList.Items.Add("Добавлено значение " + (++number).ToString());
+            slider.Maximum = lstList.Items.Count;
+        }
+
+        private void slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            lstList.SelectedIndex = Convert.ToInt32(slider.Value);
+        }
+    }
+}
